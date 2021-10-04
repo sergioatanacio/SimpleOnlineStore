631ca35e7609d4013eeca567924b52c474dac6b9
sergioAtanacio <sergio14camino@gmail.com>
2021-10-03 20:39:05 -0500

    Pequeños cambios

--- git_shell.php
+++ git_shell.php
@@ -6,9 +6,16 @@ $branch = $argv[1] ?? 'main';
 
 shell_exec("git add .");
 echo 'Escribe el mensaje:'.PHP_EOL;
-$message = readline() ?? 'Pequeños cambios';
+$message = function()
+{
+    $messageFa = readline();
+    return $messageFa !== '' ? $messageFa : 'Pequeños cambios';
+};
 
-shell_exec("git commit -m \"". $message."\"");
+
+
+var_dump($message);
+shell_exec("git commit -m \"". $message()."\"");
 shell_exec("git push origin ". $branch);
 
 // shell_exec('cd public && php -S localhost:8000 && cd .. &');
