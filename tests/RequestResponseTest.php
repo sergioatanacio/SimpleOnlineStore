342011ef5e8768c1c29008dece25131b622c2f26
sergio14camino <sergio14camino@gmail.com>
2021-10-08 22:41:18 -0500

    Empezando tdd

--- tests/RequestResponseTest.php
+++ tests/RequestResponseTest.php
@@ -6,17 +6,23 @@ use PHPUnit\Framework\TestCase;
  * $ composer dump
  * $ vendor/phpunit/phpunit/phpunit
  */
+Use App\RequestResponse\Request;
 
 class RequestResponseTest extends TestCase
 {
     public function test_request_response()
     {
+        $request = new Request();
+        $request->setInstance('UserController', 'login', null, null);
+
         
+
+
         /* 
         $rutaInicial = Route::get('', 'UserController@login', null, "/", [], null);
 
         $this->assertTrue(is_string($rutaInicial));
- */
+         */
 
         #$comment = new Comment();
 
